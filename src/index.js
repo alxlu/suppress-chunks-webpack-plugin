8addd3ddfe56da5f23c07f9a22e88c41b69ac9f7
Alex Lu <alxlu@users.noreply.github.com>
2016-12-05 11:47:20 -0800

    add flow annotations and some optional params for the constructor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+/* @flow */
 import 'core-js/modules/es7.array.includes';
 
+type F = Array<Object> | Array<string> | string;
+type O = { filter: string | Array<string>; keep: boolean };
+
 export default class SuppressEntryChunksPlugin {
-  constructor(files) {
-    this.files = Array.isArray(files) ? files : [files];
+  filter: Array<string>;
+  keep: boolean;
+  files: Array<{ name: string; extension: string | void}>;
+  constructor(files: F, { filter, keep }: O = { filter: [], keep: false }) {
+    const fileList: Array<any> = Array.isArray(files) ? files : [files];
+    this.filter = Array.isArray(filter) ? filter : [filter];
+    this.files = fileList.map((file) => {
+      if (typeof file === 'string') {
+        return { name: file, extension: void 0 };
+      }
+      return file;
+    });
+    this.keep = keep;
   }
 
-  apply(compiler) {
+  apply(compiler: Object) {
     compiler.plugin('emit', (compilation, callback) => {
       compilation.chunks.forEach((chunk) => {
-        if (this.files.includes(chunk.name)) {
+        if (this.files.map(file => file.name).includes(chunk.name)) {
           // eslint-disable-next-line no-param-reassign
           chunk.files.forEach(file => delete compilation.assets[file]);
         }
