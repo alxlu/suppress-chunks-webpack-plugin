e87175e0035a9b946a3e90358fcccfc01fc082f6
Alex Lu <alxlu@users.noreply.github.com>
2019-01-25 21:30:44 -0800

    update for webpack 4

--- src/index.js
+++ src/index.js
@@ -22,31 +22,34 @@ export default class SuppressEntryChunksPlugin {
   }
 
   apply(compiler: Object) {
-    compiler.plugin('emit', (compilation, callback) => {
-      compilation.chunks.forEach(chunk => {
-        if (this.files.map(file => file.name).includes(chunk.name)) {
-          const { match, keep } = find(this.files, { name: chunk.name });
+    compiler.hooks.emit.tapAsync(
+      this.constructor.name,
+      (compilation, callback) => {
+        compilation.chunks.forEach(chunk => {
+          if (this.files.map(file => file.name).includes(chunk.name)) {
+            const { match, keep } = find(this.files, { name: chunk.name });
 
-          chunk.files
-            .filter(file => {
-              if (match !== null) {
-                const regexp = new RegExp(match);
-                return keep ? !regexp.test(file) : regexp.test(file);
-              } else if (this.filter !== null) {
-                const regexp = new RegExp(this.filter);
-                return this.keep ? !regexp.test(file) : regexp.test(file);
-              }
-              return true;
-            })
-            .forEach(file => {
-              // eslint-disable-next-line no-param-reassign
-              chunk.files = chunk.files.filter(f => f !== file);
-              // eslint-disable-next-line no-param-reassign
-              delete compilation.assets[file];
-            });
-        }
-      });
-      callback();
-    });
+            chunk.files
+              .filter(file => {
+                if (match !== null) {
+                  const regexp = new RegExp(match);
+                  return keep ? !regexp.test(file) : regexp.test(file);
+                } else if (this.filter !== null) {
+                  const regexp = new RegExp(this.filter);
+                  return this.keep ? !regexp.test(file) : regexp.test(file);
+                }
+                return true;
+              })
+              .forEach(file => {
+                // eslint-disable-next-line no-param-reassign
+                chunk.files = chunk.files.filter(f => f !== file);
+                // eslint-disable-next-line no-param-reassign
+                delete compilation.assets[file];
+              });
+          }
+        });
+        callback();
+      }
+    );
   }
 }
