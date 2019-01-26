7d1c8b2680fe957c1f1a68732ae7458c99f6b5ac
Alex Lu <alxlu@users.noreply.github.com>
2019-01-25 21:18:24 -0800

    add prettier; udpate deps
    update webpack
    replace extract-text-webpack-plugin with mini-css-extract-plugin

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import 'core-js/modules/es7.array.includes';
 import find from 'lodash/find';
 
 type F = Array<Object> | Array<string> | string;
-type O = { filter: string | null; keep: boolean };
+type O = { filter: string | null, keep: boolean };
 
 export default class SuppressEntryChunksPlugin {
   filter: string | null;
   keep: boolean;
-  files: Array<{ name: string; match: string | null; keep: boolean}>;
+  files: Array<{ name: string, match: string | null, keep: boolean }>;
   constructor(files: F, { filter, keep }: O = { filter: null, keep: false }) {
     const fileList: Array<any> = Array.isArray(files) ? files : [files];
     this.filter = filter;
-    this.files = fileList.map((file) => {
+    this.files = fileList.map(file => {
       if (typeof file === 'string') {
         return { name: file, match: null, keep: false };
       }
@@ -23,25 +23,27 @@ export default class SuppressEntryChunksPlugin {
 
   apply(compiler: Object) {
     compiler.plugin('emit', (compilation, callback) => {
-      compilation.chunks.forEach((chunk) => {
+      compilation.chunks.forEach(chunk => {
         if (this.files.map(file => file.name).includes(chunk.name)) {
           const { match, keep } = find(this.files, { name: chunk.name });
 
-          chunk.files.filter((file) => {
-            if (match !== null) {
-              const regexp = new RegExp(match);
-              return keep ? !regexp.test(file) : regexp.test(file);
-            } else if (this.filter !== null) {
-              const regexp = new RegExp(this.filter);
-              return this.keep ? !regexp.test(file) : regexp.test(file);
-            }
-            return true;
-          }).forEach((file) => {
-            // eslint-disable-next-line no-param-reassign
-            chunk.files = chunk.files.filter(f => f !== file);
-            // eslint-disable-next-line no-param-reassign
-            delete compilation.assets[file];
-          });
+          chunk.files
+            .filter(file => {
+              if (match !== null) {
+                const regexp = new RegExp(match);
+                return keep ? !regexp.test(file) : regexp.test(file);
+              } else if (this.filter !== null) {
+                const regexp = new RegExp(this.filter);
+                return this.keep ? !regexp.test(file) : regexp.test(file);
+              }
+              return true;
+            })
+            .forEach(file => {
+              // eslint-disable-next-line no-param-reassign
+              chunk.files = chunk.files.filter(f => f !== file);
+              // eslint-disable-next-line no-param-reassign
+              delete compilation.assets[file];
+            });
         }
       });
       callback();
